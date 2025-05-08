8cd90e92297ac155578a0e24b544384b99a1c03d
MuhammadHaritsAdha <mharitsadha@gmail.com>
2025-05-08 19:41:19 +0700

    13

--- Singly_LinkedList.cpp
+++ Singly_LinkedList.cpp
@@ -134,3 +134,23 @@ int main()
             mhs.addNode();
             break;
         }
+
+        case '2':
+        {
+            if (mhs.listEmpty())
+            {
+                cout << endl
+                     << "List Kosong" << endl;
+                break;
+            }
+            cout << endl
+                 << "\nMasukkan no Mahasiswa yang akan dihapus : ";
+            cin >> nim;
+            if (mhs.delNode(nim) == false)
+                cout << endl
+                     << "Data tidak ditemukan" << endl;
+            else
+                cout << endl
+                     << "Data dengan nomor mahasiswa " << nim << "Berhasil dihapus" << endl;    
+        }
+ 
